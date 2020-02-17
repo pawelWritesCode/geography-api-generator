15bd5f3a87d92ebfcab64d73d93fb66539d3741c
Paweł Chmielewski <pawel.chmielewski@edulab.io>
2020-02-17 08:48:43 +0100

    Updated build.sh and readme

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ echo "Building generator,"
 go build main.go
 
 echo "Moving binary to binary folder,"
-mv main $GOPATH/bin/aiqa-generator2
+mv main $GOPATH/bin/geo-generator
 
-echo "Changing privilages to binary."
-sudo chmod 741 $GOPATH/bin/aiqa-generator2
+echo "Changing privilages to binary file."
+sudo chmod 741 $GOPATH/bin/geo-generator
